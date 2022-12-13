3327ba4e86aca10c71f8bf85e7ea355b0298163f
Aldis Sarja <aldis.sarja@gmail.com>
2022-12-13 10:11:02 +0200

    More pagination

--- api/views.py
+++ api/views.py
@@ -7,12 +7,17 @@ from django.core.paginator import Paginator
 
 @api_view(['GET'])
 def home(request):
-    paginator = Paginator(Article.objects.all(), 10)
     page_number = request.GET.get('page')
+    rows_per_page = request.GET.get('rows-per-page')
+    if rows_per_page == None:
+        rows_per_page = 10
+    paginator = Paginator(Article.objects.all(), rows_per_page)
     articles = paginator.get_page(page_number)
 
     serializer = ArticleSerializer(articles, many=True)
+#    return Response(serializer.data)
     return Response({
         'total_pages': paginator.num_pages,
+        'total_rows': paginator.count,
         'articles': serializer.data,
     })
